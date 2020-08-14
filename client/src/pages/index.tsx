e4bbb502d72cb6580ae805d2eb440d3cbe419676
Gabriel Brito <mfkrgabriel@gmail.com>
2020-08-14 14:53:41 -0300

    Add Prettier

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -4,14 +4,14 @@ type Props = {
   title: string
 }
 
-export default function Home({ title = 'Tell me why' } : Props) {
+export default function Home({ title = 'Tell me why' }: Props) {
   return (
     <div>
       <Head>
         <title>{title}</title>
-        <link rel='icon' href='/favicon.ico' />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
       <h1>{title}</h1>
     </div>
-  );
+  )
 }
