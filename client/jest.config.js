e938ba8c63f1b408407550f496d0c83c2f7aab42
Gabriel Brito <mfkrgabriel@gmail.com>
2020-08-17 17:24:08 -0300

    Add Styled Components and Configure Jest File

--- client/jest.config.js
+++ client/jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   testEnvironment: 'jsdom',
-  testPathIgnorePatters: ['/node_modules/', '/.next/'],
+  testPathIgnorePatterns: ['/node_modules/', '/.next/'],
   collectCoverage: true,
   collectCoverageFrom: ['src/**/*.ts(x)'],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts']
